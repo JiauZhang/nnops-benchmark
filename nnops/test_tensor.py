e170fd42da5bc42a01f004b9f9f08a653c5b6aee
JiauZhang <1743960454@qq.com>
2025-01-07 23:59:44 +0800

    update

--- nnops/test_tensor.py
+++ nnops/test_tensor.py
@@ -1,8 +1,30 @@
 import pyperf
-import time
+import nnops.tensor, nnops.ops
+import numpy as np
 
-def func():
-    time.sleep(0.001)
+np_a = np.random.randn(8, 3, 512, 512)
+np_b = np.random.randn(8, 3, 512, 512)
+nps_a = nnops.tensor.from_numpy(np_a)
+nps_b = nnops.tensor.from_numpy(np_b)
+np_a_stride = np_a[2::3, ::2, ::3, 1::2]
+np_b_stride = np_b[2::3, ::2, ::3, 1::2]
+nps_a_stride = nps_a[2::3, ::2, ::3, 1::2]
+nps_b_stride = nps_b[2::3, ::2, ::3, 1::2]
+
+def np_add():
+    _ = np_a + np_b
+
+def nps_add():
+    _ = nnops.ops.add(nps_a, nps_b)
+
+def np_add_stride():
+    _ = np_a_stride + np_b_stride
+
+def nps_add_stride():
+    _ = nnops.ops.add(nps_a_stride, nps_b_stride)
 
 runner = pyperf.Runner()
-runner.bench_func('sleep', func)
+runner.bench_func('np_add', np_add)
+runner.bench_func('nps_add', nps_add)
+runner.bench_func('np_add_stride', np_add_stride)
+runner.bench_func('nps_add_stride', nps_add_stride)
